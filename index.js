5647f379ec92dffdcb6bcbdbec6ec1a0d3c36d51
Lakhan Samani <lakhan.m.samani@gmail.com>
2021-06-08 10:49:30 +0530

    update response

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+  res.send(`👋 Hello World!`);
 });
 
 app.listen(port, () => {
