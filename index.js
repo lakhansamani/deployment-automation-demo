35765f0883b2f828cfa36e07398006d8a1aae6b8
Lakhan Samani <lakhan.m.samani@gmail.com>
2021-06-08 14:43:37 +0530

    update demo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send(`👋 Hello World ${new Date()}`);
+  res.send(`👋 Hello World from video demo`);
 });
 
 app.listen(port, () => {
